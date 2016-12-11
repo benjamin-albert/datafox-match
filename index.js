871ffda1edbea7c869cd6c053b299cac8b19c3ba
Benjamin Albert <benjamin_a5@yahoo.com>
2016-12-11 09:49:22 -0800

    Makes ambiguity a derived property

--- index.js
+++ index.js
@@ -8,39 +8,35 @@ function normalize(name) {
   return name.toLowerCase().replace(/[^a-z0-9 ]/gi, '');
 }
 
+function ambiguity(records) {
+  return Object.keys(records).length - 1;
+}
+
 function tokenizeCompanies(companies) {
   var tokenMap = {};
 
   var db = require('./db.json');
 
-  db.forEach(function(rec) {
-    var matches = {};
-
+  db.forEach(function(record) {
     function addToken(token) {
-      if (!matches[token]) {
-        matches[token] = 1;
-        var obj = tokenMap[token] || (tokenMap[token] = {});
-
-        obj.ambiguity = (obj.ambiguity + 1) || 0;
-        obj.records = obj.records || [];
-        obj.records.push(rec.id);
-      }
+      var obj = tokenMap[token] || (tokenMap[token] = {});
+      obj[record.id] = record;
     }
 
     function tokenize(company) {
       normalize(company).split(' ').forEach( addToken );
     }
 
-    tokenize(rec.name);
-    rec.corporate_names.forEach(tokenize);
-    rec.fka_names.forEach(tokenize);
+    tokenize(record.name);
+    record.corporate_names.forEach(tokenize);
+    record.fka_names.forEach(tokenize);
   } );
 
   var uniqueTokens = {};
   for (var token in tokenMap) {
     var obj = tokenMap[token];
-    if (obj.ambiguity < 8) {
-      uniqueTokens[token] = obj.records;
+    if (ambiguity(obj) < 8) {
+      uniqueTokens[token] = Object.keys(obj);
     }
   }
 
