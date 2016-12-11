78974d47bc3ad6d7b2904468bf711a9eb0feec4f
Benjamin Albert <benjamin_a5@yahoo.com>
2016-12-11 10:07:05 -0800

    Refactors index.js to use a streaming JSON parser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const JSONStream = require('JSONStream');
 const parse = require('./lib/parse');
 
 var csvFile = fs.createReadStream('crm.csv');
@@ -13,43 +14,59 @@ function ambiguity(records) {
 }
 
 function tokenizeCompanies(companies) {
+  // Maps a token to a map of databade records.
   var tokenMap = {};
 
-  var db = require('./db.json');
+  var dbPath = require.resolve('./db.json');
+  var db = fs.createReadStream(dbPath);
 
-  db.forEach(function(record) {
+  db.pipe(JSONStream.parse('*'))
+    .on('data', onRecord)
+    .on('end', removeAmbiguous);
+
+  function onRecord(record) {
     function addToken(token) {
       var obj = tokenMap[token] || (tokenMap[token] = {});
       obj[record.id] = record;
     }
 
     function tokenize(company) {
-      normalize(company).split(' ').forEach( addToken );
+      normalize(company).split(' ').forEach(addToken);
     }
 
     tokenize(record.name);
     record.corporate_names.forEach(tokenize);
     record.fka_names.forEach(tokenize);
-  } );
-
-  var uniqueTokens = {};
-  for (var token in tokenMap) {
-    var obj = tokenMap[token];
-    if (ambiguity(obj) < 8) {
-      uniqueTokens[token] = Object.keys(obj);
-    }
   }
 
-  var matches = {};
-  companies.forEach( company => {
-   normalize(company).split(' ').forEach( word => {
-     var match = uniqueTokens[word];
-     if (match) {
-       matches[company] = match;
-     }
-   });
-  });
+  function removeAmbiguous() {
+    var uniqueTokens = {};
+    for (var token in tokenMap) {
+      var obj = tokenMap[token];
+      if (ambiguity(obj) < 8) {
+        uniqueTokens[token] = Object.keys(obj);
+      }
+    }
 
-  console.log(matches);
-  console.log(Object.keys(matches).length);
+    match(uniqueTokens);
+  }
+
+  function match(uniqueTokens) {
+    var matches = {};
+    companies.forEach( company => {
+     normalize(company).split(' ').forEach( word => {
+       var match = uniqueTokens[word];
+       if (match) {
+         matches[company] = match;
+       }
+     });
+    });
+
+    printResults(matches);
+  }
+
+  function printResults(matches) {
+    console.log(matches);
+    console.log(Object.keys(matches).length);
+  }
 }
